1644cd0ed926de48f5cf7cce11ff5562f3fd27a4
Gowtham <gowtham.m6795@gmail.com>
2019-07-22 17:04:35 +0530

    Initial Commit

---
+++ Project39(XCTest)Tests/Project39_XCTest_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Project39_XCTest_Tests.swift
+//  Project39(XCTest)Tests
+//
+//  Created by dJAX on 22/07/19.
+//  Copyright © 2019 dJAX. All rights reserved.
+//
+
+import XCTest
+@testable import Project39_XCTest_
+
+class Project39_XCTest_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
